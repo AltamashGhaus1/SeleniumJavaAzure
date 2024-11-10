6f20e41e4bdee3dee851f7df4112854da3ea1de7
AltamashGhaus1 <115972084+AltamashGhaus1@users.noreply.github.com>
2024-11-10 22:55:46 +0530

    Update NewLogin.java

--- src/main/java/com/jdbc/automation/executor/Common/NewLogin.java
+++ src/main/java/com/jdbc/automation/executor/Common/NewLogin.java
@@ -18,6 +18,7 @@ public class NewLogin {
     public static Boolean newlogin(WebDriver driver, List<Records> listOb,String username,String password,ClientVersionDao clientVersionDao,int san_aut_id)  throws Exception {
 
         //My name is Aafrin Parween Bro
+        //To Pull
         Records record = new Records();
         Boolean actualOutcome = false;
         List<File> screenShot = new ArrayList<>();
